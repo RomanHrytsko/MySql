666a1088912998eaea35fc61b7251013702163d6
Roman Hrytsko <72077761+RomanHrytsko@users.noreply.github.com>
2021-02-18 01:31:56 +0200

    Update mongo.sql

--- mongo.sql
+++ mongo.sql
@@ -42,6 +42,9 @@ db.getCollection('students').aggregate([
         }
     }
 8) Знайти топ 3 учнів
+
+db.getCollection('students').find({}).sort({"avgScore": -1}).limit(3)
+
 9) Знайти середній бал по школі
 db.getCollection('students').aggregate([
 
@@ -77,12 +80,53 @@ db.getCollection('students').aggregate([
 
        
 ])
+
 12) Знайти дітей з не повною сімєю
+
+db.getCollection('students').find({
+    parents: {$size: 1}
+    })
 
 
 13) Знайти батьків які не працюють
+
+db.getCollection('students').find({
+    "parents.$[].profession": {$exists:false}
+    })
+
 14) Не працюючих батьків влаштувати офіціантами
+
+db.getCollection('students').update(
+{
+    "parents.profession": {$exists: false}
+},
+{$set:{
+    "parents.profession": "waiter"}
+},
+{multi: true}
+)
+
 15) Вигнати дітей, які мають середній бал менше ніж 2.5
+
+db.getCollection('students').remove({"avgScore": {$lt: 2.5}})
+
+
 16) Дітям, батьки яких працюють в освіті ( teacher ) поставити 5
+
+db.getCollection('students').update(
+{"parents.profession": "teacher"},
+{$set:{"avgScore": 5}},
+{multi: true}
+)
 17) Знайти дітей які вчаться в початковій школі (до 5 класу) і вивчають фізику ( physics )
+
+db.getCollection('students').aggregate([
+{
+    $match:{
+        "lessons": "physics",
+        "class":{$lte: 2}}
+        
+    }
+])
+
 18) Знайти найуспішніший клас
